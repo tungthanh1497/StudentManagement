0ecf0774aecd1d8c2e5876ceb37d99b3ba0b3e92
tungtt <tungtt@ttc-solutions.com>
2020-10-05 10:05:28 +0700

    init UpdateStudent screen
    Signed-off-by: tungtt <tungtt@ttc-solutions.com>

---
+++ app/src/main/java/com/ttc/tungtt/sm/ui/updatestudent/UpdateStudentFragment.java
@@ -0,0 +1,36 @@
+package com.ttc.tungtt.sm.ui.updatestudent;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.ttc.tungtt.sm.R;
+
+public class UpdateStudentFragment extends Fragment {
+
+    private UpdateStudentViewModel mViewModel;
+
+    public static UpdateStudentFragment newInstance() {
+        return new UpdateStudentFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.update_student_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UpdateStudentViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ttc/tungtt/sm/ui/updatestudent/UpdateStudentViewModel.java
@@ -0,0 +1,7 @@
+package com.ttc.tungtt.sm.ui.updatestudent;
+
+import androidx.lifecycle.ViewModel;
+
+public class UpdateStudentViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
