8084bfcfa3ddfe12502831ce53b3b4e43e80d274
tungtt <tungtt@ttc-solutions.com>
2020-10-05 14:11:41 +0700

    init spinner classes and genders
    Signed-off-by: tungtt <tungtt@ttc-solutions.com>

--- app/src/main/java/com/ttc/tungtt/sm/ui/updatestudent/UpdateStudentFragment.java
+++ app/src/main/java/com/ttc/tungtt/sm/ui/updatestudent/UpdateStudentFragment.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,16 +17,48 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.ttc.tungtt.sm.R;
 import com.ttc.tungtt.sm.commons.Constants;
+import com.ttc.tungtt.sm.commons.adapters.SimpleSpinnerAdapter;
 import com.ttc.tungtt.sm.databases.entities.StudentEntity;
+import com.ttc.tungtt.sm.models.SimpleModel;
 import com.ttc.tungtt.sm.models.SubjectModel;
 import com.ttc.tungtt.sm.models.TranscriptModel;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UpdateStudentFragment extends Fragment {
 
     private static final String TAG = UpdateStudentFragment.class.getSimpleName();
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface SPINNER_TYPE {
+        String GENDER = "gender";
+        String CLASS = "class";
+    }
+
+
+    @BindView(R.id.spn_gender)
+    Spinner genderSpinner;
+    @BindView(R.id.spn_class)
+    Spinner classSpinner;
+
+
     private UpdateStudentViewModel mViewModel;
+
+    private List<SimpleModel> mGenderList;
+    private List<SimpleModel> mClassList;
+
+    private SimpleSpinnerAdapter mGenderAdapter;
+    private SimpleSpinnerAdapter mClassAdapter;
+
+    private int mSelectedGenderIndex = -1;
+    private int mSelectedClassIndex = -1;
+
 
     public static UpdateStudentFragment newInstance() {
         return new UpdateStudentFragment();
@@ -32,7 +67,9 @@ public class UpdateStudentFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.update_student_fragment, container, false);
+        View view = inflater.inflate(R.layout.update_student_fragment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
@@ -41,6 +78,8 @@ public class UpdateStudentFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(UpdateStudentViewModel.class);
 
         observeLiveData();
+        initSpinners();
+        initData();
 
         mViewModel.getAllStudent();
 
@@ -50,12 +89,78 @@ public class UpdateStudentFragment extends Fragment {
         transcriptModels.add(new TranscriptModel(new SubjectModel(3, "chem"), 7));
         mViewModel.addStudent(new StudentEntity("tung",
                 "trinh thanh",
-                Constants.GENDER.MALE,
+                1,
                 1,
                 transcriptModels));
 
         new Handler().postDelayed(() -> mViewModel.getAllStudent(), 2000);
 
+    }
+
+    private void initData() {
+        initGenderList();
+        initClassList();
+    }
+
+    private void initClassList() {
+        mViewModel.getAllClass().observe(getViewLifecycleOwner(), classList -> {
+            mClassList.addAll(classList);
+            mClassAdapter.notifyDataSetChanged();
+            if (mClassList.size() > Constants.SIZE_EMPTY) {
+                classSpinner.setSelection(0);
+                mSelectedClassIndex = 0;
+            }
+        });
+    }
+
+    private void initGenderList() {
+        mViewModel.getAllGender().observe(getViewLifecycleOwner(), genderList -> {
+            mGenderList.addAll(genderList);
+            mGenderAdapter.notifyDataSetChanged();
+            if (mGenderList.size() > Constants.SIZE_EMPTY) {
+                genderSpinner.setSelection(0);
+                mSelectedGenderIndex = 0;
+            }
+        });
+    }
+
+
+    private void initSpinners() {
+        mGenderList = new ArrayList<>();
+        mClassList = new ArrayList<>();
+
+        mGenderAdapter = new SimpleSpinnerAdapter(getActivity(), mGenderList);
+        mClassAdapter = new SimpleSpinnerAdapter(getActivity(), mClassList);
+
+        bindAdapter(SPINNER_TYPE.GENDER, mGenderList, mGenderAdapter, genderSpinner);
+        bindAdapter(SPINNER_TYPE.CLASS, mClassList, mClassAdapter, classSpinner);
+    }
+
+    private void bindAdapter(String type,
+                             List<SimpleModel> list,
+                             SimpleSpinnerAdapter adapter,
+                             Spinner spinner) {
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (type) {
+                    case SPINNER_TYPE.GENDER:
+                        mSelectedGenderIndex = position;
+                        break;
+                    case SPINNER_TYPE.CLASS:
+                        mSelectedClassIndex = position;
+                        break;
+                }
+                Toast.makeText(getActivity(), "onItemSelected: " + list.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.w(TAG, "onNothingSelected: ");
+            }
+        });
     }
 
     private void observeLiveData() {
--- app/src/main/java/com/ttc/tungtt/sm/ui/updatestudent/UpdateStudentViewModel.java
+++ app/src/main/java/com/ttc/tungtt/sm/ui/updatestudent/UpdateStudentViewModel.java
@@ -4,9 +4,13 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.ttc.tungtt.sm.commons.Constants;
 import com.ttc.tungtt.sm.databases.AppDatabase;
+import com.ttc.tungtt.sm.databases.entities.ClassEntity;
+import com.ttc.tungtt.sm.databases.entities.GenderEntity;
 import com.ttc.tungtt.sm.databases.entities.StudentEntity;
 
 import java.util.List;
@@ -35,5 +39,40 @@ public class UpdateStudentViewModel extends AndroidViewModel {
         }
         return studentLiveData;
     }
-    // TODO: Implement the ViewModel
+
+    public LiveData<List<ClassEntity>> getAllClass() {
+        if (Constants.SIZE_EMPTY == mAppDatabase.classDAO().getCount()) {
+            initClassList();
+        }
+        return mAppDatabase.classDAO().getAll();
+    }
+
+    public void initClassList() {
+        addClass(new ClassEntity("11a1"));
+        addClass(new ClassEntity("11a2"));
+        addClass(new ClassEntity("11a3"));
+        addClass(new ClassEntity("11a4"));
+        addClass(new ClassEntity("11a5"));
+    }
+
+    private void addClass(ClassEntity classModel) {
+        mAppDatabase.classDAO().add(classModel);
+    }
+
+    public LiveData<List<GenderEntity>> getAllGender() {
+        if (Constants.SIZE_EMPTY == mAppDatabase.genderDAO().getCount()) {
+            initGenderList();
+        }
+        return mAppDatabase.genderDAO().getAll();
+    }
+
+    public void initGenderList() {
+        addGender(new GenderEntity("Unknown"));
+        addGender(new GenderEntity("Male"));
+        addGender(new GenderEntity("Female"));
+    }
+
+    private void addGender(GenderEntity genderModel) {
+        mAppDatabase.genderDAO().add(genderModel);
+    }
 }
